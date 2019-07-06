13974b6e88b01e308a465194b917d3e41076866e
Ignacio <shoik.xtazy@gmail.com>
2019-07-06 02:39:51 -0400

    adding states to show

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// function Helloworld(props){
+//   console.log(props);
+//   return(
+//     <div id="hello">
+//      <h3>{props.subtitle}</h3>
+//     { props.mytext }
+//     </div>
+//   );
+// }
+
+class Helloworld extends React.Component{
+
+  state = {
+    show: true
+  }
+
+  render(){
+    if(this.state.show){
+      return (
+        <div id="hello">
+        <h3>{this.props.subtitle}</h3>
+        { this.props.mytext }
+        <button>Toggle show</button>
+        </div>
+      )
+    }else{
+      return <h1>There are no elements </h1>
+    }
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>This is my component: <Helloworld mytext="Ignacio" subtitle="To Santiago"/> <Helloworld mytext="seba" subtitle="To Ukranie" /> <Helloworld mytext="osvaldo" subtitle="to Concepcion Center"/> </div>
   );
 }
 
