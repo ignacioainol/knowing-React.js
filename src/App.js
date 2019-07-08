32ccde5eeb7a3d760853017f962a47578f68422e
Ignacio <shoik.xtazy@gmail.com>
2019-07-07 23:41:09 -0400

    sample with json

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-// function Helloworld(props){
-//   console.log(props);
-//   return(
-//     <div id="hello">
-//      <h3>{props.subtitle}</h3>
-//     { props.mytext }
-//     </div>
-//   );
-// }
-
-class Helloworld extends React.Component{
-
-  state = {
-    show: true
-  }
-
-  toggleShow(){
-    this.setState({ show: false });
-   }
-
+class App extends Component{
   render(){
-    if(this.state.show){
-      return (
-        <div id="hello">
-        <h3>{this.props.subtitle}</h3>
-        { this.props.mytext }
-        <button onClick={ this.toggleShow.bind(this) }>Toggle show</button>
-        </div>
-      )
-    }else{
-      return <h1>There are no elements </h1>
-    }
+    return <h1>Hello world</h1>
   }
 }
 
-function App() {
-  return (
-   <div>This is my component: <Helloworld mytext="Ignacio" subtitle="To Santiago"/> <Helloworld mytext="seba" subtitle="To Ukranie" /> <Helloworld mytext="osvaldo" subtitle="to Concepcion Center"/> </div>
-  );
-}
 
 export default App;
