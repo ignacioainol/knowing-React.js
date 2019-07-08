bce1ca1af1611a27227a62a69d50fe3e39d39a9e
Ignacio <shoik.xtazy@gmail.com>
2019-07-08 14:45:01 -0400

    if ternario for style inside functions to css

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+import './Task.css';
 
 class Task extends Component{
+
+    StyleCompleted(){
+        return{
+            fontSize: '20px',
+            color: this.props.task.done ? 'gray' : 'black',
+            textDecoration: this.props.task.done ? 'line-through' : 'none' 
+        }
+    }
+
     
     render(){
         const {task} = this.props;
-        return  <div>
+
+        return  <p style={this.StyleCompleted()}>
                 {task.title} - 
                 {task.description} - 
                 {task.id}
             <input type="checkbox" />
-            <button>x</button>
-        </div>
+            <button style={btnDelete}>x</button>
+        </p>
     }
-} 
+}
+
+const btnDelete = {
+    fontSize: '18px',
+    background: '#ea2027',
+    color: '#fff',
+    border: 'none',
+    padding: '10px 15px',
+    borderRadius: '50%',
+    cursor: 'pointer'
+}
 
 export default Task;
